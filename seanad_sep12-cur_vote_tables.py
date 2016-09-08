a7fb9f8ae42cb9083412f165acc22adf5d795da9
MattMalis <malis.matt@gmail.com>
2016-09-08 00:06:53 -0500

    seanad sep12-present votes extracted successfully

--- seanad_sep12-cur_vote_tables.py
+++ seanad_sep12-cur_vote_tables.py
@@ -30,12 +30,13 @@ for yr in range(2012,2017):
 	this_yr_months = os.listdir(base_path+str(yr))
 	for mo in this_yr_months[1:]:
 		if yr==2012:
-			if mo not in ['September', 'October', 'November', 'December']:
+			if mo not in ['September', 'October', 'November']:#, 'December']:
 				continue
 		this_months_days = os.listdir(base_path+str(yr)+'/'+mo)
 		for day in this_months_days[1:]:
 			print '\n\n TODAY: %s%s%s' %(yr,mo,day)
 			day_file_names = import_one_day_htmls(yr, mo, day)
+			print '#htmls: %s' %len(day_file_names)
 			one_day_vote_tables = []
 			for f_name in day_file_names:
 				page = open(base_path+str(yr)+'/'+mo+'/'+day+'/'+f_name)
@@ -43,39 +44,50 @@ for yr in range(2012,2017):
 				tables = soup.find_all('table')
 				for t in range(len(tables)):
 					if len(tables[t].find_all('table'))!=0:
+						print 'table in a table! day: %s%s%s' %(yr,mo,day)
 						one_day_vote_tables.extend(tables[t].find_all('table'))
-				for i in range(len(one_day_vote_tables)):
-					try:
-						#print 'Vote table #%s: length=%s'%(i,len(one_day_vote_tables[i]))
-						vt = one_day_vote_tables[i]
-						ta_vote_tds = vt.find_all('td',{'bgcolor':'#ccffcc'}) 
-						nil_vote_tds = vt.find_all('td',{'bgcolor':'#ffcccc'})			
-						#print '\n TA VOTES:'
-						ta_names = []
-						for row in ta_vote_tds:
-							s_row = str(row)
-							begindex = s_row.find('pid=')
-							if begindex==-1:
-								continue
-							endex = s_row[begindex:].find('&amp')
-							name = s_row[begindex+len('pid='):begindex+endex]
-							#print name
-							ta_names.append(name)
-						#print '\n\n NIL VOTES:'
-						c_writer.writerow([yr, mo, day, i, 'TA']+ta_names)
-						nil_names = []
-						for row in nil_vote_tds:
-							s_row = str(row)
-							begindex = s_row.find('pid=')
-							if begindex==-1:
-								continue
-							endex = s_row[begindex:].find('&amp')
-							name = s_row[begindex+len('pid='):begindex+endex]
-							#print name
-							nil_names.append(name)
-						c_writer.writerow([yr, mo, day, i,'NIL']+ta_names)
-					except:
-						print 'ERROR: %s%s%s #%s' %(yr,mo,day,i)
+			for i in range(0,len(one_day_vote_tables)):
+				print 'range is: %s' %(range(0,len(one_day_vote_tables)))
+				if (i==0):
+					print 'len(one_day_vote_tables): %s'%(len(one_day_vote_tables))
+				try:
+					#print 'Vote table #%s: length=%s'%(i,len(one_day_vote_tables[i]))
+					vt = one_day_vote_tables[i]
+					ta_vote_tds = vt.find_all('td',{'bgcolor':'#ccffcc'}) 
+					nil_vote_tds = vt.find_all('td',{'bgcolor':'#ffcccc'})			
+					#print '\n TA VOTES:'
+					ta_names = []
+					for row in ta_vote_tds:
+						s_row = str(row)
+						begindex = s_row.find('pid=')
+						if begindex==-1:
+							continue
+						endex = s_row[begindex:].find('&amp')
+						name = s_row[begindex+len('pid='):begindex+endex]
+						#print name
+						ta_names.append(name)
+					#print '\n\n NIL VOTES:'
+					if len(ta_names)==0:
+						continue
+					ta_vote_info = [yr, mo, day, i, 'TA']
+					ta_vote_info.extend(ta_names)
+					c_writer.writerow(ta_vote_info)
+					nil_names = []
+					for row in nil_vote_tds:
+						s_row = str(row)
+						begindex = s_row.find('pid=')
+						if begindex==-1:
+							continue
+						endex = s_row[begindex:].find('&amp')
+						name = s_row[begindex+len('pid='):begindex+endex]
+						#print name
+						nil_names.append(name)
+					if len(nil_names)==0:
+						continue
+					c_writer.writerow([yr, mo, day, i,'NIL']+(nil_names))
+				except:
+					print 'ERROR: %s%s%s #%s' %(yr,mo,day,i)
+					pass
 
 c.flush()
 c.close()
