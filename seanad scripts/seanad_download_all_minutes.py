38f74bea8d0f20483f4cb6aee35f44ac993618fe
MattMalis <malis.matt@gmail.com>
2016-09-14 20:25:47 -0500

    annotated download_all_minutes scripts

--- seanad scripts/seanad_download_all_minutes.py
+++ seanad scripts/seanad_download_all_minutes.py
@@ -6,144 +6,72 @@ import os
 import csv
 import re
 from bs4 import BeautifulSoup
-#from odf.opendocument import OpenDocumentText
-#from odf.text import P
 
-
-## all of the individual day URLs are actually opendocument format...
-# had to install odfpy
-## https://pypi.python.org/pypi/odfpy
-## NEVERMIND just need to add "http://"
-
-
+### csv file below has rows of format: 
+## [year, month, day, url for this day's first page, number of pages]
 urls_csv = open('seanad_single_date_urls.csv')
 url_reader = csv.reader(urls_csv)
 
 single_days = list(url_reader)
-#cur = 'http://oireachtasdebates.oireachtas.ie/debates%20authoring/debateswebpack.nsf/takes/dail2016051700001?opendocument'
-ticker = 0
-## adding http:// to each day's url, and appending the number of pages as the last col of the day row
-for day in single_days[1:]:
-	ticker+=1
-	try:
-		day_url = 'http://' + day[-1]
-		day_page = urllib2.urlopen(day_url)
-		day_soup = BeautifulSoup(day_page.read(), "html.parser")
-		day_txt = str(day_soup)
-		select_index = day_txt.find('</select>')
-		num_index = select_index + len('</select> of ')
-		endex = day_txt[num_index:].find('\n')
-		n_pages = day_txt[num_index:num_index+endex]
-		day[-1] = day_url
-		day.append(n_pages)
-		if ticker%100==0:
-			print "ptI: %s" %(ticker)
-		#print "num_index: %s, endex: %s, n_pages: %s" %(num_index,endex,n_pages)
-	except:
-		print "ERROR WITH ptI %s%s%s" %(day[0],day[1],day[2])
-		time.sleep(random.uniform(0,5))
-		continue
 
-print "\n\ncompleted part I\n\n"
-
-c = open('seanad_urls_pages.csv', 'wb')
-c_writer = csv.writer(c)
-c_writer.writerow(["Year", "Month", "Day", "URL", "NumPages"])
-
-for day in single_days[1:]:
-	try:
-		c_writer.writerow(day)
-	except:
-		print "ERROR WITH ptII %s%s%s" %(day[0],day[1],day[2])
-		time.sleep(random.uniform(0,5))
-		continue
-
-print "\n\ncompleted part II\n\n"
-
-c.flush()
-c.close()
-
-urls_csv.flush()
 urls_csv.close()
 
-### closed the csv's, now re-opening
-cc = open('seanad_urls_pages.csv')
-cc_reader = csv.reader(cc)
-days = list(cc_reader)
 
-## looking through each page for a given day, finding whether it has a table; if so, return it (as one url in a list)
-def find_vote_table_pages(day_row):
-	day_url_p1 = day_row[-2]
-	#print day_url_p1
-	end_index = day_url_p1.find('?opendocument')
-	#print end_index
-	if end_index==-1:
-		print 'ERROR ptIII' ## FIX ME
-	workable_url = day_url_p1[:end_index-3] 
-	## workable_url takes three additional digits at end, to specify page# (there won't be >999 pages)
-	pages = day_row[-1]
-	pages_with_tables = {}
-	for p in range(1,int(pages)+1):
-		if len(str(p))==1:
-			p3 = '00%s'%(p)
-		if len(str(p))==2:
-			p3 = '0%s'%(p)
-		if len(str(p))==3:
-			p3 = str(p)
-		this_page = workable_url+p3 + '?opendocument'
-		this_page_html = urllib2.urlopen(this_page)
-		this_page_soup = BeautifulSoup(this_page_html.read(), 'html.parser')
-		page_txt = str(this_page_soup)
-		## look for some things that indicate theres a vote
-		ta_index = page_txt.find('T\xc3\xa1,') ## 'Ta' with the accent
-		nil_index = page_txt.find('N\xc3\xadl,') ## 'Nil' with the accent
-		table_index = page_txt.find('<td class="tbr">')
-		if set([ta_index, nil_index, table_index])==set([-1]) :
-			continue
-		pages_with_tables[p] = this_page
-	day_row.append(pages_with_tables)
+#eg: http://oireachtasdebates.oireachtas.ie/debates%20authoring/debateswebpack.nsf/takes/seanad2014111300001
+## replace the four digits before '?opendocument' with the appropriate digits to indicate page number
 
 
-
-def download_page(address,path,filename,wait=5):
+def download_page(address,path,filename,wait=.1):
 	time.sleep(random.uniform(0,wait))
 	page = urllib2.urlopen(address)
 	page_content = page.read()
 	with open(path+filename, 'w') as p_html:
 		p_html.write(page_content)
 
+## function takes as args: 
+##		the url for the first page of a given date; 
+## 		the page number you want to download
+##		the index of the character in the url where the 3-digit page number begins
+## 			(this should be the same for all dates and pages)
+## returns:
+## 		modified url with the desired page number
+def same_date_next_page(date_p1_url, page_num, page_num_index):
+	workable_url = date_p1_url[:page_num_index] 
+	## workable_url takes three additional digits at end, to specify page# (there won't be >999 pages)
+	p4 = '' # p3 will be a 3-char string indicating page number
+	if len(str(page_num))==1:
+		p4 = '000%s'%(page_num)
+	if len(str(page_num))==2:
+		p4 = '00%s'%(page_num)
+	if len(str(page_num))==3:
+		p4=='0%s'%(page_num)
+	if len(str(page_num))==4:
+		p4 = str(page_num)
+	updated_url = workable_url+p4 + '?opendocument'
+	return updated_url
+
+## grabbing the index to plug into the same_date_next_page() function
+day1_url = single_days[1][3] ## url column of the 2nd row
+page_num_index_all_urls = day1_url.find('?opendocument') - 4
+
+
+######## DOING THE WORK #######
 ticker = 0
-for day in days[1:]: #row 1 is the column names
+for day in single_days[1:]: #row 0 is the column names
 	ticker+=1
-	try:
-		if ticker%100==0:
-			print "ptIV: %s" %(ticker)
-		find_vote_table_pages(day)
-		if len(day[-1])==0:
-			continue
-		day_path='/Users/iramalis/Desktop/ireland-seanad-minutes/%s/%s/%s/'%(day[0],day[1],day[2])
-		if os.path.exists(day_path)==False:
-			os.makedirs(day_path)
-		for pp in day[-1].keys():
+	if ticker%100==0: ## just to see how the download is progressing...
+		print "downloading: %s" %(ticker)
+	day_path='/Users/iramalis/Desktop/ireland-seanad-minutes/%s/%s/%s/'%(day[0],day[1],day[2])
+	if os.path.exists(day_path)==False:
+		os.makedirs(day_path)
+	for pp in range(1,int(day[-1])+1): ## day[-1]: last column, which has the number of pages
+		try:
 			f_name = 'Ireland-seanad-minutes-%s%s%s-p%s.html'%(day[0],day[1],day[2],pp)
-			p_address = day[-1][pp]
-			download_page(p_address,day_path,f_name,5)
-	except:
-		print "ERROR WITH ptIV %s%s%s" %(day[0],day[1],day[2])
-		time.sleep(random.uniform(0,5))
-		continue
+			p_address = same_date_next_page(day[3], pp, page_num_index_all_urls) ## function defined above
+			download_page(p_address,day_path,f_name,.1) ##function defined above
+		except:
+			print "ERROR DOWNLOADING %s%s%s, page #%s" %(day[0],day[1],day[2],pp)
+			time.sleep(random.uniform(0,5))
+			pass
 
 	
-## HOW TO IDENTIFY VOTE TABLES?
-## eg, see 'http://oireachtasdebates.oireachtas.ie/debates%20authoring/debateswebpack.nsf/takes/seanad1975060500004?opendocument'
-## Amendment put.
-
-# The Committee divid ed: Ta[a'], 14; Ni[i']l, 24.
-
-# ^^ what to do about typos...
-## maybe search for reg ex, 'Ta, \d; Nil, \d'?
-## for that example - the vote table is the only <td class="tbr"> tag
-
-## 'Question Put', 'Amendment Put', 'Question Declared ___',...
-## 'Ta,' ?
-## search for any one of the above - if file contains one, downlaod
